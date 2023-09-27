a75637ef450a178da66e4db4a283f2f79e6ac13b
A. R. Rahsan DEWAN <38459429+arrahsandewan25@users.noreply.github.com>
2023-09-27 15:22:37 +0600

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,20 @@
-declare function plugin(options?: Partial<{ className: string; target: 'modern' | 'legacy' }>): {
-  handler: () => void
+type AnymatchFn = (testString: string) => boolean;
+type AnymatchPattern = string|RegExp|AnymatchFn;
+type AnymatchMatcher = AnymatchPattern|AnymatchPattern[]
+type AnymatchTester = {
+  (testString: string|any[], returnIndex: true): number;
+  (testString: string|any[]): boolean;
 }
 
-declare namespace plugin {
-  const __isOptionsFunction: true
+type PicomatchOptions = {dot: boolean};
+
+declare const anymatch: {
+  (matchers: AnymatchMatcher): AnymatchTester;
+  (matchers: AnymatchMatcher, testString: null, returnIndex: true | PicomatchOptions): AnymatchTester;
+  (matchers: AnymatchMatcher, testString: string|any[], returnIndex: true | PicomatchOptions): number;
+  (matchers: AnymatchMatcher, testString: string|any[]): boolean;
 }
 
-export = plugin
+export {AnymatchMatcher as Matcher}
+export {AnymatchTester as Tester}
+export default anymatch
